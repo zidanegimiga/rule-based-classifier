8c375e5dffaca5ace71f607d102ce157586b574d
zidanegimiga <zidanegimigatemp@gmail.com>
2025-05-28 13:58:35 +0000

    define preprocessor

---
+++ utils.py
@@ -0,0 +1,16 @@
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+STOP_WORDS = set(stopwords.words('english'))
+
+def preprocess(text: str):
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    tokens = word_tokenize(text)
+    filtered = [w for w in tokens if w not in STOP_WORDS]
+    return filtered
